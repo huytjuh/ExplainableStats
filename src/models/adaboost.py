b27ab855f595504c3ac5d1f5b830229573f729d6
huy huynh <huytjuh@outlook.com>
2025-12-07 14:25:27 +0100

    adaboost added

---
+++ src/models/adaboost.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import numpy as np 
+
+class AdaBoost:
+
+    def __init__(self, n_estimators: int=10, max_depth: int=5):
+        self.n_estimators = n_estimators 
+        self.max_depth = max_depth 
+
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+
+        pass
+
+    def build_forest(self, X: pd.DataFrame, y: pd.Series):
+
+        
+
+        pass
+
+    def sample_weight(self):
+
+        pass
