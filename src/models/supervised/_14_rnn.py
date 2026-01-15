b0f433691f318a1ea4ab3d50c863cc8bd4378838
huy.huynh <huytjuh@outlook.com>
2026-01-15 23:22:28 +0100

    added rnn

--- src/models/supervised/_14_rnn.py
+++ src/models/supervised/_14_rnn.py
@@ -12,12 +12,8 @@ class RNN:
         self.tol = tol
 
         self.cache = {}
-        self.Wxh = {}
-        self.Whh = {}
-        self.Why = {}
-        self.bh = {}
-        self.by = {}
-
+        self.W = {}
+        self.b = {}
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> 'RNN':
         """Train the RNN classifier."""
@@ -33,7 +29,16 @@ class RNN:
 
     def _forward_propagation(self, X: np.ndarray) -> None:
         """Perform forward propagation through the network."""
-        self.cache = {}
+        n_samples, n_features = X.shape
+        self.cache = {'A_0': X}
+        for i in range(len(self.hidden_layers)):
+            Z = self.cache[f'A_{i}'] @ self.W[f'W_{i}'] + self.b[f'b_{i}']
+            self.cache[f'Z_{i+1}'] = Z
+            self.cache[f'A_{i+1}'] = self._activation_func(Z, method='relu')
+
+        Z_out = self.cache[f'A_{len(self.hidden_layers)}'] @ self.W['W_out'] + self.b['b_out']
+        self.cache['Z_out'] = Z_out
+        self.cache['A_out'] = self._activation_func(Z_out, method='relu')
 
         return
 
