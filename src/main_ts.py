8ac0e0ef23cd2d2290baf37ca340929aa13fd2c0
huy huynh <huytjuh@outlook.com>
2026-01-24 16:15:41 +0100

    added rnn forward propagation

--- src/main_ts.py
+++ src/main_ts.py
@@ -12,6 +12,8 @@ if __name__ == "__main__":
 
     X_train, X_test, y_train, y_test = X[:-50], X[-50:], y[:-50], y[-50:]
 
+    print(X_train)
+
     RNN = RNN(learning_rate=0.01, epochs=1000, tol=1e-5)
     RNN_fit = RNN.fit(X_train, y_train)
 
--- src/models/supervised/_14_rnn.py
+++ src/models/supervised/_14_rnn.py
@@ -1,33 +1,38 @@
 import pandas as pd
 import numpy as np
+from typing import List
 
 class RNN:
     """Recurrent Neural Network (RNN) classifier from scratch."""
     
-    def __init__(self, hidden_size: int=10, learning_rate: float=0.01, epochs: int=1000, tol: float=1e-5):
+    def __init__(self, hidden_layers: List[int]=[10], learning_rate: float=0.01, epochs: int=1000, tol: float=1e-5):
         """Initialize hyperparameters for RNN."""
-        self.hidden_size = hidden_size
+        self.hidden_layers = hidden_layers
         self.lr = learning_rate 
         self.epochs = epochs
         self.tol = tol
 
+        self.hidden_activation = 'relu'
+        self.output_activation = 'relu'
+
         self.cache = {}
         self.W = {}
+        self.Wh = {}
         self.b = {}
 
     def fit(self, X: pd.DataFrame, y: pd.Series) -> 'RNN':
         """Train the RNN classifier."""
         X = X.values if isinstance(X, pd.DataFrame) else X
         y = y.values if isinstance(y, (pd.Series, pd.DataFrame)) else y
-        n_features, n_samples = X.shape
+        n_samples, n_features = X.shape
 
-        X_seq = X.reshape(n_features, 1, n_samples)
-        y_seq = y.reshape(-1, 1)
+        y = y.reshape(-1, 1)
 
         self._initialize_weights(n_features)
         list_loss = [np.inf]
 
-        self._forward_propagation(X_seq)
+
+        self._forward_propagation(X, n_samples)
 
         return self
     
@@ -37,30 +42,33 @@ class RNN:
     
     def _initialize_weights(self, n_features: int) -> None:
         """Initialize weights and biases for the network."""
-        self.W, self.b = {}, {}
+        self.W, self.b, self.H = {}, {}, {}
         prev_layer_size = n_features
         for i in range(len(self.hidden_layers)):
             self.W[f'W_{i}'] = np.random.randn(prev_layer_size, self.hidden_layers[i]) * np.sqrt(2 / prev_layer_size)
             self.b[f'b_{i}'] = np.zeros((1, self.hidden_layers[i]))
+            self.Wh[f'Wh_{i}'] = np.random.randn(self.hidden_layers[i], self.hidden_layers[i]) * np.sqrt(2 / prev_layer_size)
             prev_layer_size = self.hidden_layers[i]
 
-        self.W['W_rec'] = np.random.randn(self.hidden_layers[-1], self.hidden_layers[-1]) * np.sqrt(2 / prev_layer_size)
-        self.b['b_rec'] = np.zeros((1, self.hidden_layers[-1]))
         self.W['W_out'] = np.random.randn(self.hidden_layers[-1], 1) * np.sqrt(2 / prev_layer_size)
         self.b['b_out'] = np.zeros((1, 1))
 
-    def _forward_propagation(self, X: np.ndarray) -> None:
+    def _forward_propagation(self, X: np.ndarray, n_samples: int) -> None:
         """Perform forward propagation through the network."""
-        n_samples, n_features = X.shape
         self.cache = {'A_0': X}
         for i in range(len(self.hidden_layers)):
-            Z = self.cache[f'A_{i}'] @ self.W[f'W_{i}'] + self.b[f'b_{i}']
-            self.cache[f'Z_{i+1}'] = Z
-            self.cache[f'A_{i+1}'] = self._activation_func(Z, method='relu')
+            self.cache[f'A_{i+1}'] = np.zeros((n_samples, self.hidden_layers[i]))
+            self.cache[f'Z_{i+1}'] = np.zeros((n_samples, self.hidden_layers[i]))
+            self.cache[f'H_{i+1}'] = np.zeros((n_samples, self.hidden_layers[i]))
 
-        Z_out = self.cache[f'A_{len(self.hidden_layers)}'] @ self.W['W_out'] + self.b['b_out']
-        self.cache['Z_out'] = Z_out
-        self.cache['A_out'] = self._activation_func(Z_out, method='relu')
+        for t in range(n_samples): 
+            for i in range(len(self.hidden_layers)):
+                Z = self.cache[f'A_{i}'][t] @ self.W[f'W_{i}'] + self.cache[f'H_{i+1}'][t] @ self.Wh[f'Wh_{i}'] + self.b[f'b_{i}']
+                self.cache[f'Z_{i+1}'][t] = Z
+                self.cache[f'A_{i+1}'][t] = self._activation_func(Z, method=self.hidden_activation)
+                self.cache[f'H_{i+1}'][t] = self.cache[f'A_{i+1}'][t]
+
+        print(self.cache)
 
         return
 
